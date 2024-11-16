d290703b899013adf38fb4df0d1588b0bcc4194c
Ofek Bashan <ofekray@gmail.com>
2024-11-16 11:24:47 +0200

    Fix cjs and esm imports

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  format: ["cjs", "esm"],
+  splitting: true,
+  cjsInterop : true,
   dts: true,
   minify: true
 })
